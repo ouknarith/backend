f40c80e63c4aae8ec14dae912c911cebb22c4b23
ouknarith <ouk.narith92@gmail.com>
2024-08-04 09:47:08 +0700

    Add script.js to githut

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// console.log(`\============================`)
+// let add = function() {
+//     let a = "Declear with let"
+//     console.log(a)
+//     return a;
+// }
+// add();
+
+// 
+
+
+// console.log(`\n=========================`)
+// let add1 = function(){
+//     let a = "Delear with let"
+//     console.log(a)
+//     return a = "From add1";
+// }
+// let a1 = add1();
+// console.log(a1)
+
+console.log(`\n==========================`)
+const total2 = function(){
+    console.log(`Delear with const`)
+    const b = 12
+    return b;
+}
+const t2 = total2();
+
+
